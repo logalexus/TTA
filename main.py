8596971a36c6884368f99786d174a53986a647e5
logalexus <logalex2001@yandex.ru>
2024-04-02 03:11:07 -0400

    stream tcp assembly v2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pyshark
+
+from datetime import datetime
 from pyshark.tshark.tshark import get_tshark_interfaces
+
+streams = {}
 
 
 class Flow():
@@ -23,35 +27,47 @@ class Flow():
         return (ipeq1 or ipeq2) and (porteq1 or porteq2)
 
 
-def assembly_streams(packets, target_port: int):
-    streams = {}
-    for pkt in packets:
-        ip = pkt.ip
-        tcp = pkt.tcp
-        flow = Flow(ip.src, ip.dst, int(tcp.srcport), int(tcp.dstport))
-        if flow.portdst == target_port or flow.portsrc == target_port:
-            if flow not in streams:
-                streams[flow] = []
-            streams[flow].append(pkt)
-    return streams
+class Stream():
+    def __init__(self, flow: Flow):
+        self.flow: Flow = flow
+        self.packets = []
+        self.creation_time = datetime.now()
 
 
-def show_streams(streams):
-    for stream in streams.values():
-        for pkt in stream:
-            if "tcp.payload" in pkt.tcp._all_fields:
-                data = pkt.tcp.payload
-                data = bytes.fromhex(data.replace(":", ""))
-                data = data.decode(errors="ignore")
-                print(data)
+def assembly_streams(pkt, target_port: int):
+    ip = pkt.ip
+    tcp = pkt.tcp
+    flow = Flow(ip.src, ip.dst, int(tcp.srcport), int(tcp.dstport))
+
+    if flow.portdst == target_port or flow.portsrc == target_port:
+        if flow in streams:
+            stream = streams[flow]
+            stream.packets.append(pkt)
+            if tcp.flags_fin == "1" and tcp.flags_ack == "1":
+                # show_stream(stream)
+                print(stream)
+                streams.pop(flow)
+        elif tcp.flags_syn == "1":
+            stream = Stream(flow)
+            stream.packets.append(pkt)
+            streams[flow] = stream
+
+
+def show_stream(stream: Stream):
+    for pkt in stream.packets:
+        if "tcp.payload" in pkt.tcp._all_fields:
+            data = pkt.tcp.payload
+            data = bytes.fromhex(data.replace(":", ""))
+            data = data.decode(errors="ignore")
+            print(data)
 
 
 # pkts = pyshark.FileCapture("./dump.pcap", use_json=False)
 # interface = get_tshark_interfaces()[4]
 print(get_tshark_interfaces())
-interface = "enp0s3"
-cap = pyshark.LiveCapture(
-    interface=interface, display_filter='tcp.port == 5000')
+interface = "eth0"
+cap = pyshark.LiveCapture(interface=interface,
+                          display_filter='tcp.port == 5000')
 cap.set_debug()
 live = cap.sniff_continuously()
 
@@ -59,7 +75,5 @@ print(interface)
 
 PKT_COUNT = 100
 
-while True:
-    packets = [next(live) for _ in range(PKT_COUNT)]
-    streams = assembly_streams(packets, target_port=5000)
-    show_streams(streams)
+for pkt in live:
+    assembly_streams(pkt, target_port=5000)
