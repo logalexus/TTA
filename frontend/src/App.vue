3a0860f5e3bba2d7a0a77a0b685ee4e6c67c8fd4
logalexus <logalex2001@yandex.ru>
2024-04-03 19:02:29 +0000

    init vue js frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        VVS
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    * {
+        margin: 0;
+        box-sizing: 0;
+        padding: 0;
+    }
+</style>
